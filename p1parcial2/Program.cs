6f7f698648c573b1e790a35ca07f6b9c281467d4
neryjm98 <neryjm98@gmail.com>
2023-04-28 18:23:55 -0600

    Agregar archivos de proyecto.

---
+++ p1parcial2/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+using System;
+
+class BatallaNaval
+{
+    static void Main()
+    {
+        int[,] tablero = new int[10, 10];
+        // Inicializamos el tablero con ceros para indicar que no hay barcos
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                tablero[i, j] = 0;
+            }
+        }
+
+        // Colocamos los barcos en el tablero de forma aleatoria
+        Random rnd = new Random();
+        int barcosColocados = 0;
+        while (barcosColocados < 5)
+        {
+            int fila = rnd.Next(10);
+            int columna = rnd.Next(10);
+            if (tablero[fila, columna] == 0)
+            {
+                // Colocamos un barco en la casilla (fila, columna)
+                tablero[fila, columna] = 1;
+                barcosColocados++;
+            }
+        }
+
+        // Comenzamos el juego
+        int intentos = 0;
+        int barcosHundidos = 0;
+        while (barcosHundidos < 5)
+        {
+            Console.WriteLine("Intento número " + (intentos + 1));
+            Console.Write("Ingrese la fila: ");
+            int fila = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la columna: ");
+            int columna = int.Parse(Console.ReadLine());
+            if (tablero[fila, columna] == 1)
+            {
+                Console.WriteLine("¡Barco hundido!");
+                tablero[fila, columna] = 2; // Marcamos la casilla como hundida
+                barcosHundidos++;
+            }
+            else if (tablero[fila, columna] == 2)
+            {
+                Console.WriteLine("Ya has disparado en esta casilla");
+            }
+            else
+            {
+                Console.WriteLine("Agua");
+            }
+            intentos++;
+        }
+
+        Console.WriteLine("¡Has ganado en " + intentos + " intentos!");
+    }
+}
